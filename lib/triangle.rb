f40d6c8d2e781be052c595a44e7391fdcff7de38
Laila Campbell <lailacampbell96@gmail.com>
2020-03-24 05:25:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,9 @@
 class Triangle
   # write code here
-  
-  attr_accessor :side_one, :side_two, :side_three
+  attr_accessor :side_a, :side_b, :side_c
 
-  def initalize (side_one, side_two, side_three)
-    @side_one = side_one
-    @side_two = side_two
-    @side_three = side_three
-  end 
-  
-end 
+  def initialize(s_a, s_b, s_c)
+    @side_a = s_a
+    @side_b = s_b
+    @side_c = s_c
+  end

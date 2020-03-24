84b6c6b81f99e42996897b93db2104b1722956b8
Laila Campbell <lailacampbell96@gmail.com>
2020-03-24 05:22:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
     elsif (@side_one + @side_two <= @side_three) || (@side_one + @side_three <= @side_two) || (@side_two + @side_three <= @side_one)
       raise TriangleError
     else
-      if((@side_one == @side_b) && (@side_b == @side_c))
+      if((@side_one == @side_two) && (@side_two == @side_three))
         :equilateral
       elsif((@side_a == @side_b) || (@side_b == @side_c) || (@side_c == @side_a))
         :isosceles

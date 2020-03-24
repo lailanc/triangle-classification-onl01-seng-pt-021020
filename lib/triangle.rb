b54b3d9da7813cae8f895d70cae275c8f67fd9d4
Laila Campbell <lailacampbell96@gmail.com>
2020-03-24 05:04:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,9 @@
 class Triangle
   # write code here
   def initalize (side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
     
   end 
   

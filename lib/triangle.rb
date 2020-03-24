e9667a47a9442bee0a21d04354ff40d68c82c38c
Laila Campbell <lailacampbell96@gmail.com>
2020-03-24 05:25:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,3 +9,19 @@ class Triangle
   end
   
 end 
+
+def kind
+    if (@side_a <= 0) || (@side_b <= 0) || (@side_c <= 0)
+      raise TriangleError
+    elsif (@side_a + @side_b <= @side_c) || (@side_a + @side_c <= @side_b) || (@side_b + @side_c <= @side_a)
+      raise TriangleError
+    else
+      if((@side_a == @side_b) && (@side_b == @side_c))
+        :equilateral
+      elsif((@side_a == @side_b) || (@side_b == @side_c) || (@side_c == @side_a))
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end

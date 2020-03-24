15a52442136994f3dce8b8f2e1ed73c820abcf03
Laila Campbell <lailacampbell96@gmail.com>
2020-03-24 05:24:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,22 +9,4 @@ class Triangle
     @side_three = side_three
   end 
   
-  def kind
-    if (@side_one <= 0) || (@side_two <= 0) || (@side_three <= 0)
-      raise TriangleError
-    elsif (@side_one + @side_two <= @side_three) || (@side_one + @side_three <= @side_two) || (@side_two + @side_three <= @side_one)
-      raise TriangleError
-    else
-      if((@side_one == @side_two) && (@side_two == @side_three))
-        :equilateral
-      elsif((@side_one == @side_two) || (@side_two == @side_three) || (@side_three == @side_three))
-        :isosceles
-      else
-        :scalene
-      end
-    end
-  end
-
-  class TriangleError < StandardError
-  end
-end
+  

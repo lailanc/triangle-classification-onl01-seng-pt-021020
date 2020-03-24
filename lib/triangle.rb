46d083ee44670858fb0a0aac42e3882a34ff27d7
Laila Campbell <lailacampbell96@gmail.com>
2020-03-24 05:12:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,8 @@
 class Triangle
   # write code here
+  
+  attr_accessor :side_one, :side_two, :side_three
+
   def initalize (side_one, side_two, side_three)
     @side_one = side_one
     @side_two = side_two
